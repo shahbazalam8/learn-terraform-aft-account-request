4c556d099f48c486ea4027b5300328bb80efcd33
Shahbaz Alam <shahbaz.alam@synectiks.com>
2022-04-05 00:34:25 +0530

    add subaccount details

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,16 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "shahbaz.alam@synectiks.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "sandbox"
+    SSOUserEmail              = "shahbaz.alam@synectiks.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Account" = "subaccount-test"
   }
 
   change_management_parameters = {
