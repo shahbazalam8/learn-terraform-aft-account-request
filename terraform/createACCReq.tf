4cfd0d76dcb7752cfb311ddcff235609156a8c78
root <shahbaz.alam@synectiks.com>
2022-04-27 17:55:45 +0000

    new accounts

---
+++ terraform/createACCReq.tf
@@ -0,0 +1,27 @@
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "shabazalam8@yahoo.com"
+    AccountName               = "stageaccount"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "shahbaz.alam@synectiks.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Account" = "subaccount-stage"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Admin"
+    change_reason       = "Staging Account for demo Application"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
