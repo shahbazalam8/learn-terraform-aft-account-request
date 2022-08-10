240c144489047b418eec7edbef0de9b6cd53c349
root <shahbaz.alam@synectiks.com>
2022-08-10 08:15:46 +0000

    new accounts for Demo

---
+++ terraform/AccCreate.tf
@@ -0,0 +1,27 @@
+module "sandbox07" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "salmankhann01234@gmail.com"
+    AccountName               = "DevSecOps"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "shahbaz.alam@synectiks.com"
+    SSOUserFirstName          = "DevSecOps"
+    SSOUserLastName           = "Ops"
+  }
+
+  account_tags = {
+    "Account" = "subaccount-devops"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Admin"
+    change_reason       = "DevSecops Application testing"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
