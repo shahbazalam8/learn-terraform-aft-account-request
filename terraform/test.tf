ebb2c97acdc82e875da46ead460ba361794541e4
Shahbaz Alam <shahbaz.alam@synectiks.com>
2022-08-10 16:46:13 +0530

    test

---
+++ terraform/test.tf
@@ -0,0 +1,27 @@
+module "sandbox_tree" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "salmankhann012345@gmail.com"
+    AccountName               = "testaccount"
+    ManagedOrganizationalUnit = "sandbox"
+    SSOUserEmail              = "shahbaz.alam@synectiks.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Account" = "subaccount-stage"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Admin"
+    change_reason       = "Staging Account for demo Application"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
