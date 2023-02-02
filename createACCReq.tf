4144f11d23843be93f81cf2134a87bfa113e63f8
Shahbaz Alam <shahbaz.alam@synectiks.com>
2023-02-02 19:31:37 +0530

    new account

--- createACCReq.tf
+++ createACCReq.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "testit" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "kalakhann01234@gmail.com"
+    AccountName               = "devtest"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "shahbaz.alam@synectiks.com"
+    SSOUserFirstName          = "adminuser"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Account" = "subaccount-stage"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Admin"
+    change_reason       = "Staging Account for demo Application"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
